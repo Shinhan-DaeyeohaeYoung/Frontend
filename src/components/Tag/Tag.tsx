785b8f3d4069f4cd8338cc1192207cb7cc992ba7
sujin <bellecode20@gmail.com>
2025-08-25 00:36:16 +0900

    Feat: 태그 컴포넌트, 스토리북 개발

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Tag as ChakraTag } from '@chakra-ui/react';
+
+export type AppTagVariant = 'default' | 'error';
+
+export interface AppTagProps {
+  label: string;
+  variant?: AppTagVariant;
+}
+
+export const Tag: React.FC<AppTagProps> = ({ label, variant = 'default' }) => {
+  const isError = variant === 'error';
+
+  const styles =
+    variant === 'error'
+      ? { color: 'red.600', borderColor: 'red.600', bg: 'white', colorPalette: 'red' }
+      : { color: 'gray.800', borderColor: 'gray.300', bg: 'white', colorPalette: 'gray' };
+
+  return (
+    <ChakraTag.Root size="md" borderRadius="md" px={3} py={1} variant="outline" {...styles}>
+      <ChakraTag.Label>{label}</ChakraTag.Label>
+    </ChakraTag.Root>
+  );
+};
+
+export default Tag;
