663da1bbc0850d4ecb81efb18d988473daf8c462
xodms8133 <xodms8133@naver.com>
2025-08-30 03:50:29 +0900

    Fix: 배포 오류 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,7 +13,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { tokenStorage } from '../api/client';
 import { postRequest, getRequest } from '../api/requests'; // 요청 유틸 함수 import
-import { toaster } from '../components/UI/toaster';
+import { toaster } from '@/components/UI/toaster';
 import { useAuthStore } from '../stores/authStore';
 
 interface LoginResponse {
